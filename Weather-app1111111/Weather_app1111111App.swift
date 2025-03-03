4905d3ca087574324c483b7c4a6e5811e742f9d6
Maimai10808 <331686549@qq.com>
2025-03-03 21:39:41 +0800

    Initial Commit

---
+++ Weather-app1111111/Weather_app1111111App.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_app1111111App.swift
+//  Weather-app1111111
+//
+//  Created by mac on 3/3/25.
+//
+
+import SwiftUI
+
+@main
+struct Weather_app1111111App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Weather-app1111111Tests/Weather_app1111111Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_app1111111Tests.swift
+//  Weather-app1111111Tests
+//
+//  Created by mac on 3/3/25.
+//
+
+import Testing
+@testable import Weather_app1111111
+
+struct Weather_app1111111Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
